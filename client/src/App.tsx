ca8f7c6bfb32914d85840e7eeef2039cd33c996a
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2024-11-14 16:06:13 +0000

    test3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>My Movie App</p>
+        <p>My Movie App ...</p>
       </header>
     </div>
   );
